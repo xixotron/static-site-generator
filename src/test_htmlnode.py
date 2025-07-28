63488b761860e606cdedfa51fa1e12697088d7d2
xixotron <xixotron89@gmail.com>
2025-07-28 17:04:30 +0200

    unified HTMLNode tests into single file like the classes

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -42,6 +42,75 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(
             "HTMLNode(tag=None, value=None, children=None, props={'href': 'boot.dev', 'target': '_blank'})",
             repr(node))
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "This is only text")
+        self.assertEqual("This is only text", node.to_html())
+
+    def test_to_html_p(self):
+        node = LeafNode("p", "This is a paragraph")
+        self.assertEqual("<p>This is a paragraph</p>", node.to_html())
+
+    def test_to_html_a(self):
+        node = LeafNode("a", "This is a link", {"href":"https://boot.dev"})
+        self.assertEqual('<a href="https://boot.dev">This is a link</a>', node.to_html())
+
+    def test_to_html_no_value_Exception(self):
+
+        node = LeafNode("p", None)  # type:ignore value should be a str
+        with self.assertRaises(ValueError) as context:
+            html = node.to_html()
+            self.assertIsNone(html)
+
+        self.assertNotIn("html", locals())
+        self.assertTrue("invalid HTML: no value" in str(context.exception))
+
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_single_child(self):
+        node = ParentNode("p", [LeafNode("","text")])
+        self.assertEqual("<p>text</p>", node.to_html())
+
+    def test_to_html_multiple_child(self):
+        node = ParentNode("p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "Italic text"),
+                LeafNode(None, "Normal text")
+            ])
+        self.assertEqual(
+            "<p><b>Bold text</b>Normal text<i>Italic text</i>Normal text</p>",
+            node.to_html())
+
+    def test_to_html_nested(self):
+        node = ParentNode(
+            "div",
+            [
+                ParentNode("span", [LeafNode("p", "Normal Text"), LeafNode("b","Bold Text")]),
+                LeafNode("i", "Italic Text")
+            ])
+        self.assertEqual(
+            "<div><span><p>Normal Text</p><b>Bold Text</b></span><i>Italic Text</i></div>",
+            node.to_html())
+
+    def test_to_html_props(self):
+        node = ParentNode("p", [LeafNode(None, "text")], {"color" : "red"})
+        self.assertEqual('<p color="red">text</p>', node.to_html())
+
+    def test_to_html_no_child_raises(self):
+        node = ParentNode("p", [])
+        with self.assertRaises(ValueError) as context:
+            node.to_html()
+        self.assertTrue("invalid HTML: no children" in str(context.exception))
+
+    def test_to_html_no_tag_raises(self):
+        node = ParentNode("", [LeafNode("","text")])
+        with self.assertRaises(ValueError) as context:
+            node.to_html()
+        self.assertTrue("invalid HTML: no tag" in str(context.exception))
 
 
 if __name__ == "__main__":
