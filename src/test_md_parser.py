8e351d80a965229cb6f922a76f6341aad430d1d7
xixotron <xixotron89@gmail.com>
2025-07-29 20:05:42 +0200

    refactored imports

--- src/test_md_parser.py
+++ src/test_md_parser.py
@@ -1,6 +1,13 @@
 import unittest
 
-from md_parser import extract_markdown_images, extract_markdown_links, split_nodes_delimiter, split_nodes_image, split_nodes_link, text_to_textnodes
+from md_parser import (
+        extract_markdown_images,
+        extract_markdown_links,
+        split_nodes_delimiter,
+        split_nodes_image,
+        split_nodes_link,
+        text_to_textnodes
+)
 
 from textnode import TextNode, TextType
 
