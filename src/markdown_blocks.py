eb9a9054bdbfa96e39475fe5bbccbe8424d57b9e
xixotron <xixotron89@gmail.com>
2025-07-29 20:57:44 +0200

    block types and block type checker + test

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,47 @@
+import re
+
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH       = "paragraph"
+    HEADING         = "heading"
+    CODE            = "code"
+    QUOTE           = "quote"
+    UNORDERED_LIST  = "unordered_list"
+    ORDERED_LIST    = "ordered_list"
+
+
+def block_to_block_type(block: str) ->BlockType:
+    matches = re.fullmatch(r"^#{1,6} [^\n]*$", block)
+    if matches:
+        return BlockType.HEADING
+
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+
+    is_quote_block = True
+    is_unordered_block = True
+    is_ordered_block = True
+    for i, line in enumerate(block.split("\n")):
+        numeral = f"{i + 1}."
+
+        is_quote_block =  is_quote_block and line.startswith("> ")
+        is_unordered_block = is_unordered_block and line.startswith("- ")
+        is_ordered_block = is_ordered_block and line.startswith(numeral)
+
+        if not (is_quote_block or is_unordered_block or is_ordered_block):
+            return BlockType.PARAGRAPH
+
+    if is_quote_block:
+        return BlockType.QUOTE
+    if is_unordered_block:
+        return BlockType.UNORDERED_LIST
+    if is_ordered_block:
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
+
 
 
 def markdown_to_blocks(markdown: str) -> list[str]:
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import BlockType, block_to_block_type, markdown_to_blocks
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -85,6 +85,116 @@ With tabs at the start and end of it\t
             markdown_to_blocks(md)
         )
 
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_heading_block(self):
+        self.assertEqual(BlockType.HEADING, block_to_block_type("# Heading block"))
+
+    def test_singe_line_code_block(self):
+        self.assertEqual(BlockType.CODE, block_to_block_type("```CODE GOES HERE```"))
+
+    def test_multi_line_code_block(self):
+        self.assertEqual(
+            BlockType.CODE,
+            block_to_block_type("```main():\n    print('Hello, world!')\nmain() # writes to console```")
+        )
+
+    def test_single_line_quote_block(self):
+        self.assertEqual(
+            BlockType.QUOTE,
+            block_to_block_type("> quotes say things someone previously said")
+        )
+
+    def test_multi_line_quote_block(self):
+        self.assertEqual(
+            BlockType.QUOTE,
+            block_to_block_type(
+"""> quotes say things someone previously said
+> some quotes have more than one line
+> others my be long
+> spread over multiple lines"""
+            )
+        )
+
+    def test_single_line_unordered_list_block(self):
+        self.assertEqual(
+            BlockType.UNORDERED_LIST,
+            block_to_block_type("- There could be single entry lists")
+        )
+
+    def test_multi_line_unordered_list_block(self):
+        self.assertEqual(
+            BlockType.UNORDERED_LIST,
+            block_to_block_type(
+"""- This is part of a list
+- Lists can have multiple entries
+- List can be long
+- This also is part of a list"""
+            )
+        )
+
+    def test_single_line_ordered_list_block(self):
+        self.assertEqual(
+            BlockType.ORDERED_LIST,
+            block_to_block_type("1. There could be single entry lists")
+        )
+
+    def test_multi_line_ordered_list_block(self):
+        self.assertEqual(
+            BlockType.ORDERED_LIST,
+            block_to_block_type(
+"""1. This is part of a list
+2. Lists can have multiple entries
+3. List can be long
+4. This also is part of a list"""
+            )
+        )
+
+    def test_single_line_paragraph_block(self):
+        self.assertEqual(
+            BlockType.PARAGRAPH,
+            block_to_block_type("There can be single paragraphs")
+        )
+
+    def test_multi_line_paragraph_block(self):
+        self.assertEqual(
+            BlockType.PARAGRAPH,
+            block_to_block_type(
+"""There can also be paragraphs
+That span multiple
+lines of text"""
+            )
+        )
+
+    def test_malformed_code_block(self):
+        self.assertEqual(
+            BlockType.PARAGRAPH,
+            block_to_block_type("```This is not a valid code block")
+        )
+
+    def test_malformed_quote_block(self):
+        self.assertEqual(
+            BlockType.PARAGRAPH,
+            block_to_block_type("> This is not a valid\n> quote block\nThe last line doesn't start with '> '")
+        )
+
+    def test_malformed_unordered_list_block(self):
+        self.assertEqual(
+            BlockType.PARAGRAPH,
+            block_to_block_type("- This is not a valid\nunordered\n- list block")
+        )
+
+    def test_malformed_ordered_list_block(self):
+        self.assertEqual(
+            BlockType.PARAGRAPH,
+            block_to_block_type("1. This is not a valid\n1. unordered\n1. list block")
+        )
+
+    def test_malformed_ordered_list_block2(self):
+        self.assertEqual(
+            BlockType.PARAGRAPH,
+            block_to_block_type("1. This is not a valid\n2. unordered\nlist block")
+        )
 
 if __name__ == "__main__":
     unittest.main()
